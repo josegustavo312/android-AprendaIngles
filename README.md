ecaccf3f5de523c802a8d16acdd1901852d43255
josegustavo312 <jose.gustavo312@yahoo.com.br>
2021-05-07 10:51:08 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Aprenda Inglês - Mobile
+
+###  :computer: Tela
+
+<div align="center">
+  <img alt="Tela" title="#Tela" width="100%" height="auto" src="./README/tela.png" />
+</div>
+
+----------
+
+### :hammer_and_wrench: Ferramentas
+As seguintes tecnologias foram utilizadas na construção do projeto:
+- Java;
+- XML;
+- Android Studio.
+
+----------
+
+###  :gear: Função
+- [x] Aplicativo para o ensino da língua inglesa, ao clicar em uma imagem é emitido um som na respectiva língua.
+
+----------
+
+###  :eyeglasses: Autor
+José Gustavo da Silva.
+[
+**Linkedin**
+](https://www.linkedin.com/in/jose-gustavo312/)
---
+++ app/src/main/java/com/jgs/aprendaingles/MainActivity.java
@@ -0,0 +1,43 @@
+package com.jgs.aprendaingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.jgs.aprendaingles.Fragments.BichosFragment;
+import com.jgs.aprendaingles.Fragments.NumerosFragment;
+import com.jgs.aprendaingles.Fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0); // Remove a sombra do ActionBar.
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Bichos", BichosFragment.class)
+                        .add("Números", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
+                .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
+    }
+}
